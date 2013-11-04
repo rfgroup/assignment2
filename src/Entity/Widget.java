c896bceeb53ab87d4035514e0b3d58e4d7b032b1
Mike Haas <mikehaas763@gmail.com>
2013-11-04 10:37:21 -0600

    Add package for entities

---
+++ src/Entity/Widget.java
@@ -0,0 +1,11 @@
+package Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mike
+ * Date: 11/4/13
+ * Time: 10:31 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Widget {
+}
