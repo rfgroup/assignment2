8491d53b0c183b9c6f3e2edb0a57745a65eba89c
aaroncook06 <aaroncook06@gmail.com>
2013-11-05 14:52:25 -0600

    Jlist now loads widgets into gui

--- src/WidgetOrder/WidgetOrder.java
+++ src/WidgetOrder/WidgetOrder.java
@@ -11,6 +11,7 @@ package WidgetOrder;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.persistence.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -26,34 +27,51 @@ import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.Label;
+import java.util.Collection;
+import java.util.List;
+import java.util.Vector;
+
 import javax.swing.JLabel;
 
-public class WidgetOrder extends JFrame {
 
+
+import WidgetOrder.Entity.Widget;
+
+public class WidgetOrder extends JFrame {
+	
+	
+	
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_1;
-
+	private JList<Widget> listView;
+	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					WidgetOrder frame = new WidgetOrder();
+
+					//Fetching Widgets from DB to load into JList
+					new CreateFixtures().run();
+					Vector<Widget> items = new Vector<Widget>();
+					final String PERSISTENCE_UNIT_NAME = "WidgetOrders";
+					EntityManagerFactory factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+				    EntityManager em = factory.createEntityManager();
+				  
+				    Collection<Widget> widgets = em.createNamedQuery("Widget.findAll", Widget.class).getResultList();
+			        for (Widget widget : widgets) {
+			            items.add(widget);
+			        }
+				   
+					WidgetOrder frame = new WidgetOrder(items);
 					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public WidgetOrder() {
+	public WidgetOrder(Vector<Widget> input_list) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 603, 366);
 		contentPane = new JPanel();
@@ -65,13 +83,11 @@ public class WidgetOrder extends JFrame {
 		scrollBar.setBounds(396, 30, 19, 261);
 		contentPane.add(scrollBar);
 		
-		JList list = new JList();
-		list.setBounds(77, 206, 169, -125);
-		contentPane.add(list);
+	
 		
-		JList list_1 = new JList();
-		list_1.setBounds(33, 30, 381, 261);
-		contentPane.add(list_1);
+		listView = new JList<Widget>(input_list);
+		listView.setBounds(33, 30, 381, 261);
+		contentPane.add(listView);
 		
 		JButton btnProcessOrder = new JButton("Process Order");
 		btnProcessOrder.setBounds(424, 268, 123, 23);
@@ -99,4 +115,6 @@ public class WidgetOrder extends JFrame {
 		lblEnterCustomerName.setBounds(424, 125, 137, 14);
 		contentPane.add(lblEnterCustomerName);
 	}
+	
+	        
 }
